c052d286347f4e0f43fd37e84d5bf6de11bacff2
Adarsha Naik <adarshanaiksiddapura@gmail.com>
2026-04-25 13:43:12 +0530

    CORS update for production

--- CityPlus/Backend/src/server.js
+++ CityPlus/Backend/src/server.js
@@ -15,7 +15,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'https://citypluss.netlify.app',
+  credentials: true
+}));
 app.use(express.json());
 
 
